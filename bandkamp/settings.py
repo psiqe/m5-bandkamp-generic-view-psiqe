42de25efc154e5590ce7d537f4ad985c25e248e7
Murillo Miguel <mmiguel.skn@gmail.com>
2022-12-16 14:48:55 -0300

    fix:allowed_hosts

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["web-production-248a.up.railway.app", "0.0.0.0"]
+ALLOWED_HOSTS = ["web-production-86ca.up.railway.app", "0.0.0.0"]
 
 
 # Application definition
